fbb85b584c414ebf21a8eed957a42504b46eff6e
soozbank <soozbank@gmail.com>
2021-03-06 15:20:53 +0500

    'update'

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -7,10 +7,9 @@ module.exports.sendMail = async (options) =>
 
       let transporter = nodemailer.createTransport({
         host: 'smtp.office365.com',
-        port: '587',
+        port: 587,
+        secure: false,
         auth: { user: process.env.OUTLOOK_USER, pass: process.env.OUTLOOK_PASSWORD },
-        secureConnection: false,
-        tls: { ciphers: 'SSLv3' },
       });
 
       const mailOptions = {
